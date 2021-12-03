eae3d4aa04664ae8fe3872d8108ae741f7942722
fofcn <errorfatal89@gmail.com>
2021-12-03 18:27:53 +0800

    1、去重算法变更

--- Week3/collinear/FastCollinearPoints.java
+++ Week3/collinear/FastCollinearPoints.java
@@ -11,13 +11,14 @@ import edu.princeton.cs.algs4.StdOut;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class FastCollinearPoints {
     private int num = 0;
     private Point[] points;
     private List<LineSegment> segmentList;
-
+    private final HashSet<String> duplicateTest = new HashSet<>();
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
         if (points == null) {
@@ -50,38 +51,33 @@ public class FastCollinearPoints {
 
     // the line segments
     public LineSegment[] segments() {
-        List<Point[]> startPoints = new ArrayList<>();
-
         Point[] origin = Arrays.copyOf(points, points.length);
         for (int i = 0; i < points.length; i++) {
             if (i + 1 >= points.length) {
                 break;
             }
-            Arrays.sort(origin, points[i].slopeOrder());
             // 对排序数组中的相同斜率进行搜索
+            Arrays.sort(origin, points[i].slopeOrder());
+
             // 如果与原点有相同的斜率，那么就记录
-            // 如果斜率相同的技术大于等于三，那就存在四个或更多点共线
-            // 记录最小点和最大点
+            // 如果斜率相同的计数大于等于三，那就存在四个或更多点共线
+            // 排序后取第一个元素为最小点和最后一个元素为最大点
             List<Point> foundPoints = new ArrayList<>();
             double startSlop = points[i].slopeTo(origin[i + 1]);
             for (int j = 1; j < origin.length; j++) {
                 double slope = points[i].slopeTo(origin[j]);
                 // 检查斜率是否与origin数组中的第一个元素的斜率是否相同
-                // 如果相同，那么更新最大和最小元素的值
-                // 斜率相同的个数加1
-                // 记录end的值加1
-                // 如果相同数量大于等于3次，那么存在4个共线的点
                 if (startSlop == slope) {
                     foundPoints.add(origin[j]);
                 } else {
-                    addSegment(startPoints, i, foundPoints);
+                    addSegment(i, foundPoints);
                     foundPoints.clear();
                     foundPoints.add(origin[j]);
                     startSlop = slope;
                 }
             }
 
-            addSegment(startPoints, i, foundPoints);
+            addSegment(i, foundPoints);
         }
 
         // 去重
@@ -90,30 +86,21 @@ public class FastCollinearPoints {
         return result;
     }
 
-    private void addSegment(List<Point[]> startPoints, int i, List<Point> foundPoints) {
+    private void addSegment(int i, List<Point> foundPoints) {
         if (foundPoints.size() >= 3) {
             num++;
             foundPoints.add(points[i]);
             Collections.sort(foundPoints);
-
-            // 检查起点是否已经存在
-            boolean exists = false;
-            for (Point[] point : startPoints) {
-                if (point[0].compareTo(foundPoints.get(0)) == 0 &&
-                        point[1].compareTo(foundPoints.get(foundPoints.size() - 1)) == 0) {
-                    exists = true;
-                }
+            Point start = foundPoints.get(0);
+            Point end = foundPoints.get(foundPoints.size() - 1);
+            String key = start.toString() + end.toString();
+            if (duplicateTest.contains(key)) {
+                return;
             }
-
-            if (!exists) {
-                Point[] segement = new Point[2];
-                segement[0] = foundPoints.get(0);
-                segement[1] = foundPoints.get(foundPoints.size() - 1);
-                startPoints.add(segement);
-                LineSegment lineSegment = new LineSegment(foundPoints.get(0),
-                                                          foundPoints.get(foundPoints.size() - 1));
-                segmentList.add(lineSegment);
-            }
+            duplicateTest.add(key);
+            LineSegment lineSegment = new LineSegment(foundPoints.get(0),
+                                                      foundPoints.get(foundPoints.size() - 1));
+            segmentList.add(lineSegment);
         }
     }
 
